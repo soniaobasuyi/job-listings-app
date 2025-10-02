c2b1ebee741163fc3b738fe8e73fe279335510fc
soniaobasuyi <obasuyisonia@gmail.com>
2025-10-02 18:47:57 +0100

    adding forms and v-model

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
   const message = ref('Hello world!');
   const status = ref('active');
   const tasks = ref(['one', 'two', 'three', 'four']);
+  const newTask = ref('');
 
   const toggleStatus = () => {
     if (status.value === 'active') {
@@ -14,15 +15,37 @@ import { ref } from 'vue';
       status.value = 'active';
     }
   };
+
+  const addTask = () => {
+    if (newTask.value !== '') {
+      tasks.value.push(newTask.value);
+      newTask.value = '';
+    }
+  };
+
+  const deleteTask = (index) => {
+    tasks.value.splice(index, 1);
+  };
 </script>
 
 <template>
   <h1>{{ message }}</h1>
   <p>user is {{ status }}</p>
 
+  <form @submit.prevent='addTask'>
+    <label for='newTask'>Add Task</label>
+    <input type='text' id='newTask' name='newTask' v-model='newTask' />
+    <button type='submit'>Submit</button>
+  </form>
+
   <h3>Tasks: </h3>
   <ul>
-    <li v-for='task in tasks' :key='task'>{{ task }}</li>
+    <li v-for='(task, index) in tasks' :key='task'>
+      <span>
+        {{ task }}
+      </span>
+      <button @click='deleteTask(index)'>x</button>
+    </li>
   </ul>
 
   <button @click='toggleStatus'>Change status</button>
