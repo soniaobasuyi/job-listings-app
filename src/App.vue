90ead2ac66effa2b9fb80efdc68d39e55c488e41
soniaobasuyi <obasuyisonia@gmail.com>
2025-10-02 18:18:46 +0100

    cleanup and shorten logic

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
-<script>
+<script setup>
 import { ref } from 'vue';
 
-export default {
-  setup() {
-    const message = ref("Hello world!");
-    const status = ref("active");
-    const tasks = ref(["one", "two", "three", "four"]);
+  const message = ref("Hello world!");
+  const status = ref("active");
+  const tasks = ref(["one", "two", "three", "four"]);
 
-    const toggleStatus = () => {
-      if (status.value === "active") {
-        status.value = "pending";
-      } else if (status.value === "pending") {
-        status.value = "inactive";
-      } else {
-        status.value = "active";
-      }
-    };
-
-    return {message, status, tasks, toggleStatus};
-  }
-};
+  const toggleStatus = () => {
+    if (status.value === "active") {
+      status.value = "pending";
+    } else if (status.value === "pending") {
+      status.value = "inactive";
+    } else {
+      status.value = "active";
+    }
+  };
 </script>
 
 <template>
