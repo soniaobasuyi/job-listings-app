5562e9ae41baed0fa08890e6e152b7f9dced0a9f
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-05 15:24:41 +0100

    completed job listing section

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,15 @@
 <script setup>
-import jobData from "@/jobs.json";
-import { ref } from "vue";
-import JobListing from "@/components/JobListing.vue";
+import jobData from '@/jobs.json';
+import { ref, defineProps } from 'vue';
+import JobListing from '@/components/JobListing.vue';
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 const jobs = ref(jobData.jobs);
 </script>
@@ -9,16 +17,20 @@ const jobs = ref(jobData.jobs);
 <template>
   <section class="bg-blue-50 px-4 py-10">
     <div class="lg:container m-auto">
-      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
-        Browse Jobs
-      </h2>
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
       </div>
     </div>
-  </section>
 
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-3 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a>
+    </section>
+  </section>
 </template>
 
 <style scoped></style>
