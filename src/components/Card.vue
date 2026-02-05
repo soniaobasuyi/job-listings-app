98c2fadfca8c72efe3370001f08ceba984bd8cfc
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-05 13:00:27 +0100

    added homecards + card container

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
 
-<template></template>
+defineProps({
+  bg: {
+    type: String,
+    default: "bg-gray-100",
+  },
+});
+</script>
+
+<template>
+  <div v-bind:class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
 
 <style scoped></style>
