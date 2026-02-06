0a08ba0e7d8162dfb882b90328376b3137a4a4f5
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-06 20:02:38 +0100

    minor refactor

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -23,13 +23,13 @@ const jobs = ref(jobData.jobs);
         <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
       </div>
     </div>
+  </section>
 
-    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
-        class="block bg-black text-white text-center py-3 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a>
-    </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="/jobs"
+      class="block bg-black text-white text-center py-3 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a>
   </section>
 </template>
 
