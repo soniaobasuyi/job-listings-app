7bc1b643b0487511ad1e5f728a646121d39ea35a
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-06 19:52:25 +0100

    added and configured vue router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
 import 'primeicons/primeicons.css';
+import router from '@/router/index.js';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
-
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
-  $END$
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
