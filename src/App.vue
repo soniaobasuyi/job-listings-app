90c35d1a3a7cced70168b986fdf15c8cd83d7ebb
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-06 20:29:56 +0100

    custom footer added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import { RouterView } from "vue-router";
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <footer class="mx-4 my-3 text-gray-300 hover:underline text-xs">
+    <RouterLink to="https://github.com/soniaobasuyi/job-listings-app">
+      © Sonia Obasuyi 2026 || Vue Tutorial Project with Traversy Media
+    </RouterLink>
+  </footer>
+</template>
+
+<style scoped></style>
