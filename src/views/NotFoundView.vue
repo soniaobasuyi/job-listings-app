412b3c796fbb1f5097bf0d63967c922bdadb67f9
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-09 17:51:40 +0100

    notfound page added

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,11 +1,19 @@
 <script setup>
-
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  $END$
+  <section class="text-center flex flex-col justify-center items-center h-96">
+    <i class="pi pi-exclamation-triangle text-yellow-500 text-7xl mb-5"></i>
+    <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
+    <p class="text-xl mb-5">This page does not exist</p>
+    <RouterLink
+      to="/"
+      class="text-white bg-green-700 hover:bg-green-900 rounded-md px-3 py-2 mt-4"
+      >
+      Go Back
+    </RouterLink>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
