0de3acc593970ac3c5e0493402e82e7579608cc9
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-09 17:35:13 +0100

    added visibility for active links in the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <script setup>
 import logo from '@/assets/img/logo.png';
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
+
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+};
 </script>
 
 <template>
@@ -17,19 +22,37 @@ import { RouterLink } from 'vue-router';
             <div class="flex space-x-2">
               <RouterLink
                 to="/"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white',
+                  'text-white',
+                  'px-3',
+                  'py-2',
+                  'rounded-md',
+                ]"
               >
                 Home
               </RouterLink>
               <RouterLink
                 to="/jobs"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/jobs') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white',
+                  'text-white',
+                  'px-3',
+                  'py-2',
+                  'rounded-md',
+                ]"
               >
                 Jobs
               </RouterLink>
               <RouterLink
                 to="/jobs/add"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/jobs/add') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white',
+                  'text-white',
+                  'px-3',
+                  'py-2',
+                  'rounded-md',
+                ]"
               >
                 Add Job
               </RouterLink>
