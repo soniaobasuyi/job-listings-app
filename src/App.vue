4e74370e4e4db4541ae61b191f83f8996b755d4d
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-09 17:50:05 +0100

    minor refactor to the footer visibility

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import { RouterView } from "vue-router";
-import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
   <Navbar />
   <RouterView />
-  <Footer />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import Hero from '@/components/Hero.vue';
 import HomeCards from '@/components/HomeCards.vue';
 import JobListings from '@/components/JobListings.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
   <Hero />
   <HomeCards />
   <JobListings :limit="3" :showButton="true" />
+  <Footer />
 </template>
 
 <style scoped></style>
