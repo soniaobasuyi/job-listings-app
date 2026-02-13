37ceb1bd669e9c340e6cd303d291f0636082e620
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-13 16:37:04 +0100

    vue toast notification added

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
 import 'primeicons/primeicons.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import router from '@/router/index.js';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(Toast);
 
 app.mount('#app');
--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive } from 'vue';
 import router from '@/router';
+import { useToast } from 'vue-toastification';
 import axios from 'axios';
 
 const form = reactive({
@@ -16,6 +17,8 @@ const form = reactive({
     contactPhone: '',
   },
 });
+
+const toast = useToast();
 
 const handleSubmit = async () => {
   const newJob = {
@@ -34,11 +37,11 @@ const handleSubmit = async () => {
 
   try {
     const response = await axios.post('/api/jobs', newJob);
-    // @todo - show toast
+    toast.success('Job Successfully Created!');
     await router.push(`/jobs/${response.data.id}`);
   } catch (error) {
     console.log('error submitting form', error);
-    // @todo - show toast
+    toast.error('Error creating Job. Please try again.');
   }
 };
 </script>
