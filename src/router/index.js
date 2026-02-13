b2fb591034ad185fd93405caf33e1d0999222c8d
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-13 20:54:22 +0100

    added functionality to edit job

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from '@/views/AddJobView.vue';
+import EditJobView from '@/views/EditJobView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,9 +25,14 @@ const router = createRouter({
       component: JobView,
     },
     {
-      path:'/jobs/add',
+      path: '/jobs/add',
       name: 'add-job',
       component: AddJobView,
+    },
+    {
+      path: '/jobs/edit/:id',
+      name: 'edit-job',
+      component: EditJobView,
     },
     {
       path: '/:catchAll(.*)',
